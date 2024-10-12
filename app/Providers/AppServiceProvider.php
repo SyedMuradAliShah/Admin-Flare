6fc97af9c49db0118633b03103def2a9988d4169
Syed Murad Ali Shah <murad@recmail.net>
2024-10-12 18:24:25 +0500

    Schema is check is added to provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ namespace App\Providers;
 use Override;
 use App\Models\Setting;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,17 +16,23 @@ class AppServiceProvider extends ServiceProvider
      * Register any application services.
      */
     #[Override]
-    public function register(): void {}
+    public function register() : void
+    {
+    }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot() : void
     {
 
-        $settings = Cache::rememberForever('site_settings', fn () => Setting::all()->pluck('value', 'key')->toArray());
+        if (Schema::hasTable('settings'))
+        {
 
-        config(['setting' => $settings]);
+            $settings = Cache::rememberForever('site_settings', fn () => Setting::all()->pluck('value', 'key')->toArray());
+
+            config([ 'setting' => $settings ]);
+        }
 
     }
 }
